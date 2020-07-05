d9efb7fa84c42097ba0defd6729d42acbe571c82
kkyung <hoi88ki@yahoo.com.hk>
2020-07-05 11:28:19 +0800

    Added Home Leader Fetch

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,6 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 
-import { DISHES } from '../shared/dishes';
+// import { DISHES } from '../shared/dishes';
 
 import { baseUrl } from '../shared/baseUrl';
 
@@ -147,4 +147,41 @@ export const promosFailed = (errmess) => ({
 export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
+});
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading());
+
+    return fetch(baseUrl + 'leaders')
+    .then(response => {
+        // console.log(response);
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(leaders => dispatch(addLeaders(leaders)))
+    .catch(error => dispatch(leadersFailed(error.message)));
+};
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
+});
+
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING
+})
+
+export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errmess
 });
